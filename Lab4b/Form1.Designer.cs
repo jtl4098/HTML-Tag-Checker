2be813b7a44913493b1df5a6420fe5af96fed1c0
Taekyung Kil <000799798@mohawkcollege.ca>
2020-02-01 19:46:58 -0500

    first_commit

---
+++ Lab4b/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Lab4b
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkTagsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(70, 116);
+            this.textBox.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(804, 368);
+            this.textBox.TabIndex = 0;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(67, 60);
+            this.statusLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(157, 18);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "HTML FIle Checker";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.processToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(997, 33);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadFileToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadFileToolStripMenuItem
+            // 
+            this.loadFileToolStripMenuItem.Name = "loadFileToolStripMenuItem";
+            this.loadFileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.L)));
+            this.loadFileToolStripMenuItem.Size = new System.Drawing.Size(243, 34);
+            this.loadFileToolStripMenuItem.Text = "Load File";
+            this.loadFileToolStripMenuItem.Click += new System.EventHandler(this.LoadFileToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(243, 34);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // processToolStripMenuItem
+            // 
+            this.processToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkTagsToolStripMenuItem});
+            this.processToolStripMenuItem.Name = "processToolStripMenuItem";
+            this.processToolStripMenuItem.Size = new System.Drawing.Size(88, 29);
+            this.processToolStripMenuItem.Text = "Process";
+            // 
+            // checkTagsToolStripMenuItem
+            // 
+            this.checkTagsToolStripMenuItem.Enabled = false;
+            this.checkTagsToolStripMenuItem.Name = "checkTagsToolStripMenuItem";
+            this.checkTagsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.checkTagsToolStripMenuItem.Size = new System.Drawing.Size(263, 34);
+            this.checkTagsToolStripMenuItem.Text = "Check Tags";
+            this.checkTagsToolStripMenuItem.Click += new System.EventHandler(this.CheckTagsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 675);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "TK\'s Simple HTML File Checker";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkTagsToolStripMenuItem;
+    }
+}
+
---
+++ Lab4b/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/// <summary>
+/// Program : Form1.cs
+/// @author : Taekyung Kil
+/// Date : 14/Nov/2019
+/// 
+/// Purpose : To set the Form
+/// </summary>
+namespace Lab4b
+{
+
+    public partial class Form1 : Form
+    {
+        //set global variables
+        string fileName = "";
+        string fileText = "";
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        /// <summary>
+        /// this button is to load a html file using file dialog
+        /// loading file should be a html file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LoadFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox.Clear();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "html files (*.html)|*.html";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                
+                fileName = openFileDialog.FileName;
+                statusLabel.Text = $"Loaded : {fileName}";
+                StreamReader reader = new StreamReader(fileName);
+                fileText = reader.ReadToEnd();
+            }
+            checkTagsToolStripMenuItem.Enabled = true; //To enable to use check tags button after loading a file
+        }
+        /// <summary>
+        /// this button is to exit the application.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// It is used for checking html tags from html file.
+        /// users are able to check opening tags, closing tags and non-container tags.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CheckTagsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            List<string> tagList = new List<string>();
+            Stack<string> htmltag = new Stack<string>();
+            string tag = "";
+            // Store html tag into the tagList such as <html> , <title> and </html>.
+            for (int i = 0; i < fileText.Length; i++)
+            {
+                if (fileText[i] == '<')
+                {
+ 
+                    tag += fileText[i];
+                    for (int j = i + 1; j < fileText.Length; j++)
+                    {
+
+                        tag += fileText[j];
+                        if (fileText[j] == '>')
+                        {
+                            tagList.Add(tag);
+                            tag = "";
+                            break;
+                        }
+                    }
+                }
+            }
+
+
+            string[] nonContainerTags = { "<br>" }; // set non-container Tags list. it is created a list to be easy to add non-container tags
+            int indentationcount = 0; //To get indentation based on the stack list
+            // To check which tags are opening or closing or non-contatiner.
+            // if a tag contains '/', it is a closing tag and remove it in the stack list using the pop method
+            // if a tag contains "<hr>" or "<img>" or "<br>", it is a closing tag and remove it in the stack list using the pop method
+            // else a tag does not contain, the tag is a opening tag and add it in the stack list using the push method
+            for (int i = 0; i < tagList.Count; i++)
+            {
+                if (tagList[i].IndexOf('/') == 1)
+                {
+                    indentationcount--;
+                    htmltag.Pop();
+                    textBox.Text += new string(' ', indentationcount) + $"Found closing tag: {tagList[i]} \r\n";
+
+                }           
+                else if (tagList[i].Contains("<hr>"))
+                {
+                    textBox.Text += $"Found non-container tag: {tagList[i]} \r\n";
+                }              
+                else if (tagList[i].Contains("img"))
+                {
+                    
+                    tagList[i] = "<img>";                   
+                    textBox.Text += $"Found non-container tag: {tagList[i]} \r\n";
+                }
+                else if (tagList[i].Contains("href"))
+                {
+                    
+                   
+                    tagList[i] = "<a>";
+                    htmltag.Push(tagList[i]);
+                    textBox.Text += new string(' ', indentationcount) + $"Found opening tag: {tagList[i]} \r\n";
+                    indentationcount++;
+                }
+                else if (tagList[i].Contains("table"))
+                {
+                    
+                   
+                    tagList[i] = "<table>";
+                    htmltag.Push(tagList[i]);
+                    textBox.Text += new string(' ', indentationcount) + $"Found opning tag: {tagList[i]} \r\n";
+                    indentationcount++;
+
+                }
+                else if (tagList[i].Contains("<td"))
+                {
+                    
+                   
+                    tagList[i] = "<td>";
+                    htmltag.Push(tagList[i]);
+                    textBox.Text += new string(' ', indentationcount) + $"Found opning tag: {tagList[i]} \r\n";
+                    indentationcount++;
+
+                }
+                else if (nonContainerTags.Contains<string>(tagList[i]))
+                {                   
+                    textBox.Text += $"Found non-container tag: {tagList[i]} \r\n";
+
+                }
+                else
+                {
+                    textBox.Text += new string(' ', indentationcount) + $"Found opening tag: {tagList[i]} \r\n";
+                    indentationcount++;
+                    htmltag.Push(tagList[i]);
+                    
+                }
+
+              
+            }
+            // To display the html tags on text box 
+            foreach (string t in htmltag)
+            {
+                textBox.Text += t;
+            }
+
+            if (htmltag.Count > 0) // To check that the html file has balanced tags or not
+            {
+                statusLabel.Text = $"{fileName} dose not have balanced tags";
+                textBox.Text += "\r\n Need more Closing tag";
+            }
+            else
+            {
+                statusLabel.Text = $"{fileName} has balanced tags";
+            }
+         
+        }
+    } 
+}
